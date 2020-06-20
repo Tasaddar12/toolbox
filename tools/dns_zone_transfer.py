cea6eb9438b8e8b3cbb10eb89f6c76b5e331f2a6
Tasaddar12 <61332370+Tasaddar12@users.noreply.github.com>
2020-06-20 18:47:30 -0400

    Add files via upload

--- tools/dns_zone_transfer.py
+++ tools/dns_zone_transfer.py
@@ -1,27 +1,37 @@
 import dns.resolver, dns.query, dns.zone, re
+from lib.urlparsing import HTTPParse
 
-def nslookup(domain):
-	domain = re.sub(r"\/$", "", domain)
-	domain = re.sub(r"^(https://|http://)", "", domain)
-	
-	answers = dns.resolver.resolve(domain, "NS")
-	for rdata in answers:
-		dnsserver = re.sub(r"\.$", "", str(rdata.target))
-		print(dnsserver)
+class DNSAttacks:
+	def nslookup(domain):
+		#Set RawDomain
+		domain = HTTPParse.rawdomain_parse(domain)
+		#Attempt to resolve dns
 		try:
-			z = dns.zone.from_xfr(dns.query.xfr(dnsserver, domain))
-			print(z.to_text())
-			names = z.nodes.keys()
-			f = open('Logs/' + domain + '-dns_transfer.log', 'w')
-			for n in names:
-				print(str(n) + "." + domain)
-				f.write(str(n) + "." + domain + "\n")
-			f.write("\n\n")
-			f.write(z.to_text())
-			f.close()
-			break
+			answers = dns.resolver.resolve(domain, "NS")
 		except:
-			print("error")
+			answers = dns.resolver(domain, "NS")
 			pass
-	print("FINISHED")
+		#Start to filter the data
+		for rdata in answers:
+			dnsserver = re.sub(r"\.$", "", str(rdata.target))
+			print(dnsserver)
+			#Attempt Zone Transfer
+			try:
+				z = dns.zone.from_xfr(dns.query.xfr(dnsserver, domain))
+				print(z.to_text())
+				names = z.nodes.keys()
+				#Start writing to a log file
+				f = open('Logs/' + domain + '-dns_transfer.log', 'w')
+				for n in names:
+					print(str(n) + "." + domain)
+					f.write(str(n) + "." + domain + "\n")
+				f.write("\n\n")
+				f.write(z.to_text())
+				f.close()
+				break
+			except:
+				print("error")
+				pass
+		print("FINISHED")
+
 
--- tools/spider.py
+++ tools/spider.py
@@ -1,95 +1,65 @@
 #imports
 import re, requests, os
-from multiprocessing.pool import ThreadPool as Pool
+from multiprocessing.dummy import Pool as ThreadPool
 from bs4 import BeautifulSoup
+from lib.urlparsing import HTTPParse
+from lib.spider_parse import Spider_parse
+
 #START MAIN SCRIPT
-#Some settings
-temp = []
-check = []
-#Startup function
-def spider_start(url, thread, loop, depth):
-	try:
+class Spider:
+	#Startup function
+	def spiderinit(url, thread, loop, depth):
 		#Attempt to get url
 		requests.get(url).status_code
 		#Settings
-		global rawurl, dep, rawdomain
+		global rawurl, dep, rawdomain, temp, check
+		temp = []
+		check = []
 		dep = depth
-		url = re.sub(r"\/$", "", url)
-		rawurl = url
-		rawdomain = re.sub(r"^(https://|http://)", "", url)
+		rawurl = ((re.split(r'\/', url))[0] + "//" + (re.split(r'\/', url))[2])
+		rawdomain = HTTPParse.rawdomain_parse(url)
+		url = rawurl
 		print(rawdomain)
+	
 		#remove duplicate files
-		if os.path.exists("Logs/Spider_log_" + rawdomain):
-			os.remove("Logs/Spider_log_" + rawdomain)
+		#if os.path.exists("Logs/Spider_log_" + rawdomain):
+			#os.remove("Logs/Spider_log_" + rawdomain)
 		temp.append(url)
-		crawl(url)
+		Spider.crawl(url)
 		for i in range(int(loop)):
 			print("\x1b[1;32;40mStarting Loop : {}\x1b[0;38;40m".format(i + 1))
-			threading_pool(int(thread))
+			Spider.threading_pool(int(thread))
 		print("\x1b[1;36;40mFINISHED\x1b[0;38;40m")
-	except:
-		print("URL Not Available")
-####################################################Functions####################################################
-def crawl(url):
-	#Check if available and add to checks
-	check.append(url)
-	if requests.get(url).status_code == 404:
-		return
-	#add to log file
-	spider_log = open("Logs/Spider_log_" + rawdomain, 'a')
-	if re.search(r"(\?|\=)", url):
-		with open("Logs/Dynamic_URL_" + rawdomain, 'a') as dynamic:
-			dynamic.write(url + "\n")
-	else:
-		spider_log.write(url + " : " + str(requests.get(url).status_code) + "\n")
-	spider_log.close()
-	#Printing to console
-	print(url + " : \x1b[1;33;40m" + str(requests.get(url).status_code) + "\x1b[0;38;40m")
-	#Grabbing the text and parsing it
-	soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-	#Crawl Links
-	for link in soup.find_all('a'):
-		#Check if part of domain
-		if not link.get('href'):
-			continue
-		if (re.search(r"^(https://|http://)", link.get('href')) and not re.search("^.{5,15}" + rawdomain, link.get('href'))):
-			continue
-		#Check if starts with http or https
-		if re.search(r"(http://|https://)", link.get('href')):
-			if link.get('href') not in temp:
-				temp.append(link.get('href'))
-		if not re.search(r"(https://|http://)", link.get('href')):
-			if (rawurl + link.get('href')) not in temp and re.search(r"^\/", link.get('href')):
-				temp.append(rawurl + link.get('href'))
-			elif not re.search(r"^\/", link.get('href')) and (rawurl + "/" + link.get('href')) not in temp:
-				temp.append(rawurl + "/" + link.get('href'))
-	#Checking the depth
-	if int(dep) >= 2:
-		#Crawl scripts
-		for link in soup.find_all('script'):
-			if not link.get('src'):
-				continue
-			if (re.search(r"^(https://|http://)", link.get('src')) and not re.search("^.{5,15}" + rawdomain, link.get('src'))):
-				continue
-			if re.search(r"(http://|https://)", link.get('src')):
-				if link.get('src') not in temp:
-					temp.append(link.get('src'))
-			if not re.search(r"(https://|http://)", link.get('src')):
-				if (rawurl + link.get('src')) not in temp and re.search(r"^\/", link.get('src')):
-					temp.append(rawurl + link.get('src'))
-				elif not re.search(r"^\/", link.get('src')) and (rawurl + "/" + link.get('src')) not in temp:
-					temp.append(rawurl + "/" + link.get('src'))
-#Threading
-def threading_pool(thread):
-	try:
-		pool = Pool(thread)
-		for url in temp:
-			if url in check:
-				continue
-			pool.apply_async(crawl, (url,))
+	####################################################Functions####################################################
+	def crawl(url):
+		if url in check:
+			return
+		#Check if available and add to checks
+		check.append(url)
+		if requests.get(url).status_code == 404:
+			return
+		#Printing to console
+		print(url + " : \x1b[1;33;40m" + str(requests.get(url).status_code) + "\x1b[0;38;40m")
+		#Grabbing the text and parsing it
+		for link in Spider_parse.tag(url, dep):
+			if link not in temp:
+				temp.append(link)
+
+	#Threading
+	def threading_pool(thread):
+		'''try:
+			pool = Pool(thread)
+			for url in temp:
+				if url in check:
+					continue
+				pool.apply_async(Spider.crawl, (url,))
+			pool.close()
+			pool.join()
+		except KeyboardInterrupt:
+			print("\n Interrupt")
+			exit()'''
+		pool = ThreadPool(thread)
+		results = pool.map(Spider.crawl, temp)
 		pool.close()
 		pool.join()
-	except KeyboardInterrupt:
-		print("\n Interrupt")
-		exit()
 
